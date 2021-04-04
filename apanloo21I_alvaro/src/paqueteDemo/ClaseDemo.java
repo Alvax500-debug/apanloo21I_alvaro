7c013c58bb6df37d7127743e39a7f7e271c6e035
Alvax500-debug <puckye25@gmail.com>
2021-04-03 23:21:28 -0600

    Primer Commit con eclipse
    Probando integracion de GIT con Eclipse prueba 2

---
+++ apanloo21I_alvaro/src/paqueteDemo/ClaseDemo.java
@@ -0,0 +1,12 @@
+package paqueteDemo;
+
+public class ClaseDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Prueba 2 Git con Eclipse");
+
+	}
+
+}
